767d6dd6b4d1d2a3c7eaaecb77e31040df12bc0b
Lathesh Y <lathesh.y@pacewisdom.com>
2024-05-15 21:50:46 +0530

    added ssr

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ParentComponent } from './parent/parent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //  path:'',redirectTo:'parent',pathMatch:'full'
+  // },
+  // {
+  //   path:'parent',component:ParentComponent
+  // }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabledBlocking'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
